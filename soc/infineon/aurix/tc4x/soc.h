zephyrproject-rtos/zephyr

soc: infineon: add AURIX TC4x SoC support
Add SoC support for the Infineon AURIX TC4x family (TC4D7, TC4D9)
including devicetree, clock tree, peripheral mappings, Kconfig,
endinit and access protection helpers and CPU idle implementation.

Also adds the shared AURIX SoC family Kconfig and build
infrastructure used by both TC3x and TC4x.

Signed-off-by: Christoph Seitz <christoph.seitz@infineon.com>
Signed-off-by: Parthiban Nallathambi <parthiban@linumiz.com>

soc/infineon/aurix/tc4x/soc.h
@@ -1,0 +1,42 @@
+/*
+ * Copyright (c) 2024 Infineon Technologies AG
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_SOC_INFINEON_AURIX_TC4X_SOC_H_
+#define ZEPHYR_SOC_INFINEON_AURIX_TC4X_SOC_H_
+
+#include <zephyr/kernel.h>
+#include <zephyr/sys/util.h>
+#include "soc_tagid.h"
+#include "soc_prot.h"
+
+#if !defined(_ASMLANGUAGE)
+#include "IfxCpu_reg.h"
+
+static inline int aurix_enable_clock(uintptr_t clc, uint32_t timeout)
+{
+	if ((sys_read32(clc) & 0x2) == 0) {
+		return 1;
+	}
+
+	sys_write32(sys_read32(clc) & ~BIT(0), clc);
+	return WAIT_FOR((sys_read32(clc) & 0x2) == 0, timeout, k_busy_wait(1));
+}
+
+static inline int aurix_disable_clock(uintptr_t clc, uint32_t timeout)
+{
+	sys_write32(sys_read32(clc) | BIT(0), clc);
+	return WAIT_FOR((sys_read32(clc) & 0x2) == 0x2, timeout, k_busy_wait(1));
+}
+
+static inline int aurix_kernel_reset(mem_addr_t krst0, uint32_t timeout)
+{
+	sys_write32(BIT(31), krst0 + 4);
+	sys_write32(1, krst0);
+	sys_write32(1, krst0 + 4);
+	return WAIT_FOR((sys_read32(krst0 + 8) & 0x1) == 0x1, timeout, k_busy_wait(1));
+}
+
+#endif
+#endif /* ZEPHYR_SOC_INFINEON_AURIX_TC4X_SOC_H_ */

linumiz/zephyr-tricore

soc/infineon/aurix/tc4x/soc.h
@@ -1,0 +1,45 @@
+/*
+ * Copyright (c) 2024 Infineon Technologies AG
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_SOC_INFINEON_AURIX_TC4X_SOC_H_
+#define ZEPHYR_SOC_INFINEON_AURIX_TC4X_SOC_H_
+
+#include <zephyr/kernel.h>
+#include <zephyr/sys/util.h>
+
+#include "soc_tagid.h"
+#include "soc_prot.h"
+#include "soc_tos.h"
+
+#define aurix_coreid_to_tos(coreId) coreId
+
+#if !defined(_ASMLANGUAGE)
+
+static inline int aurix_enable_clock(uintptr_t clc, uint32_t timeout)
+{
+	if ((sys_read32(clc) & 0x2) == 0) {
+		return 1;
+	}
+
+	sys_write32(sys_read32(clc) & ~BIT(0), clc);
+	return WAIT_FOR((sys_read32(clc) & 0x2) == 0, timeout, k_busy_wait(1));
+}
+
+static inline int aurix_disable_clock(uintptr_t clc, uint32_t timeout)
+{
+	sys_write32(sys_read32(clc) | BIT(0), clc);
+	return WAIT_FOR((sys_read32(clc) & 0x2) == 0x2, timeout, k_busy_wait(1));
+}
+
+static inline int aurix_kernel_reset(mem_addr_t krst0, uint32_t timeout)
+{
+	sys_write32(BIT(31), krst0 + 4);
+	sys_write32(1, krst0);
+	sys_write32(1, krst0 + 4);
+	return WAIT_FOR((sys_read32(krst0 + 8) & 0x1) == 0x1, timeout, k_busy_wait(1));
+}
+
+#endif
+#endif /* ZEPHYR_SOC_INFINEON_AURIX_TC4X_SOC_H_ */